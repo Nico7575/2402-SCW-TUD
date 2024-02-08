542d9119337646cf9e823866f31f8e694494fdaf
Nico7575 <158486199+Nico7575@users.noreply.github.com>
2024-02-08 16:42:47 +0100

    count_lines.py: change name style as initial and surname
    Done in GitHub

--- count_lines.py
+++ count_lines.py
@@ -2,7 +2,7 @@
 Aim: this script counts the number of lines in standard input
 Input: strings from  the command line
 Output: I have added another line
-Author: Nicolo Magro
+Author: N. Magro
 """
 
 import sys
